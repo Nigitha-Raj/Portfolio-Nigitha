7202a6406caa543bc915bfbd63a4ce3c22913f9a
Nigitha-Raj <nigitha1797@gmail.com>
2024-11-02 22:11:01 +0530

    logos added

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ ScrollReveal().reveal('.home-contact h1,.about-img', { origin: left });
 ScrollReveal().reveal('.home-contact p,.about-content', { origin: right });
 /*********** typed Js ***************/
 const typed = new Typed('.multiple-text', {
-      strings: ['Fron-end Developer','Back-end Developer','Full stack Developer'],
+      strings: ['Full stack Developer'],
       typeSpeed: 70,
       backSpeed: 70,
       backDelay: 1000,
